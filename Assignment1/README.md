14f76917eee6eac2ee44e8df8dd54fc59f4e9165
strangeTany <strangetany@gmail.com>
2021-02-09 21:00:15 +0300

    readme

---
+++ Assignment1/README.md
@@ -0,0 +1,23 @@
+# Sentence parser and word search
+The program gets text as an input and divides it into sentences. Then it searches for sentences that contain words from input and highlights them.
+<em>Remark:</em> The program is not sensitive to a case e.g ```drop``` is not equal to ```dRoP```
+
+## Requirements
+<b>Version of the language:</b> C++11 
+<b>Input:</b> file ```in.txt``` must be in the same folder as ```main.cpp```
+<b>Output:</b> file ```out.txt``` must be in the same folder as ```main.cpp```
+
+## Input
+In the file ```in.txt``` in the first line should be a text. Sentences in the text must be separated by dots. The second line contains single number (the number of searched words). All following lines contain words (one word in each line).
+
+##### Example:
+```
+Lorem Ipsum is simply a dummy text of the printing  and  type setting  industry.   LoremIpsum  has  been  the  industry’s  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing  software  like  Aldus  PageMaker  including versions of Lorem Ipsum.
+2
+dummy
+release
+```
+## Output
+In the file ```out.txt``` for each searched word there are
+* Number of sentences containing the word
+* Sentences with searched word in uppercase. Each sentence is in a separate line
--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,85 +1,88 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-vector<string> parseSentences(string text)
-{
+/**
+ * @brief Breaks the input text into sentences
+ * 
+ * @param text string containing text that should be divided into sentences. Sentences are separated by dots.
+ * @return vector<string> containing all sentences in the original text separated into distinct strings
+ */
+
+vector<string> parseSentences(string text) {
     vector<string> sentences;
     string nextSentence;
-    for (int i = 0; i < text.size(); i++)
-    {
+
+    for (int i = 0; i < text.size(); i++) {
         nextSentence.push_back(text[i]);
-        if (text[i] == '.')
-        {
+        if (text[i] == '.') {
             sentences.push_back(nextSentence);
             nextSentence = "";
-            while (i + 1 < text.size() && text[i + 1] == ' ')
+            while (i + 1 < text.size() && text[i + 1] == ' ')  //inore all spaces after a dot
                 i++;
         }
     }
+
     return sentences;
 }
 
-vector<string> search(vector<string> sentences, string word)
-{
+/**
+ * @brief Search the word and highlight it
+ * 
+ * @param sentences Sentences where word should be searched
+ * @param word Word for search
+ * @return vector<string> All sentences containing requested word. The searched word became uppercase 
+ */
+
+vector<string> search(vector<string> sentences, string word) {
     vector<string> targetSentences;
     string wordUpperCase = word;
-    for (auto &c : wordUpperCase)
+
+    for (auto &c : wordUpperCase)  //converting original word to uppercase
         c = toupper(c);
-    for (auto sentence : sentences)
-    {
+
+    for (auto sentence : sentences) {
         char previous, next;
         string newSentence = "";
-        bool flag = false;
-        for (int i = 0; i < sentence.size() - word.size(); i++)
-        {
+        bool containsWord = false;  //indicator that searched word is contained in current sentence
+        for (int i = 0; i < sentence.size() - word.size(); i++) {
             previous = (i == 0) ? ' ' : sentence[i - 1];
             next = (i + word.size() + 1 == sentence.size()) ? ' ' : sentence[i + word.size()];
 
-            if (word == sentence.substr(i, word.size()) && previous == ' ' && (next == ' ' || next == '.'))
-            {
+            if (word == sentence.substr(i, word.size()) && previous == ' ' && (next == ' ' || next == '.')) {  //check if substring is equal to terget word, and check if this substring is a word
                 newSentence += wordUpperCase;
                 i += word.size() - 1;
-                flag = true;
-            }
-            else
-            {
+                containsWord = true;
+            } else {
                 newSentence += sentence[i];
             }
         }
-        if (flag)
+        if (containsWord)
             targetSentences.push_back(newSentence + sentence.substr(newSentence.size()));
     }
     return targetSentences;
 }
 
-int main()
-{
+int main() {
     fstream fin("in.txt");
     fstream fout("out.txt");
     string text;
     getline(fin, text);
-    vector<string> s = parseSentences(text);
-    for (auto l : s)
-    {
-        cout << l << endl;
-    }
+    vector<string> separatedSentences = parseSentences(text);
     int number;
     fin >> number;
-    for (int i = 0; i < number; i++)
-    {
+
+    for (int i = 0; i < number; i++) {
         string targetWord;
         fin >> targetWord;
-        vector<string> updatedSentences = search(s, targetWord);
+        vector<string> updatedSentences = search(separatedSentences, targetWord);
         fout << updatedSentences.size() << endl;
-        for (int j = 0; j < updatedSentences.size(); j++)
-        {
-            fout << updatedSentences[j] << " ";
+        for (int j = 0; j < updatedSentences.size(); j++) {
+            fout << updatedSentences[j] << endl;
         }
-        fout<<endl;
     }
 
     return 0;
