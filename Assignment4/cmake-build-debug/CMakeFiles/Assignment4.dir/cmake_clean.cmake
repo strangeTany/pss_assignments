f0c80e9453a5ea40143dae5bc00bbfcbba5067ce
strangeTany <strangetany@gmail.com>
2021-04-03 15:19:33 +0300

    init

---
+++ Assignment4/cmake-build-debug/CMakeFiles/Assignment4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment4"
+  "Assignment4.pdb"
+  "CMakeFiles/Assignment4.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
